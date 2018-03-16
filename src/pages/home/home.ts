8f8bf43a5bcc82fa04ad439fcf719fa70708d22b
maiiradie <2159296@slu.edu.ph>
2018-03-17 00:31:55 +0800

    fix bugs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,41 +5,41 @@ import { Geolocation } from '@ionic-native/geolocation';
 import { Observable } from 'rxjs/Observable';
 
 // import { AuthProvider } from '../../providers/auth/auth';
-import  MapboxDirections  from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
 
 import { ActionSheetController } from 'ionic-angular';
 import { ComoredetailsPage } from '../Comoredetails/Comoredetails';
 
-import { AngularFireAuth } from 'angularfire2/auth';
+// import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  public map;
-  public marker;
-  public hoMarkers;
+	public map;
+	public marker;
+	public hoMarkers;
 
-  //private authProvider:AuthProvider,
-  constructor(private afs:AngularFireAuth,private afdb:AngularFireDatabase, public actionSheetCtrl: ActionSheetController, private geolocation: Geolocation, public navCtrl: NavController,public loadingCtrl: LoadingController) {
+	//private authProvider:AuthProvider,
+	constructor(private afdb: AngularFireDatabase, public actionSheetCtrl: ActionSheetController, private geolocation: Geolocation, public navCtrl: NavController, public loadingCtrl: LoadingController) {
 		mapboxgl.accessToken = 'pk.eyJ1IjoicnlhbjcxMTAiLCJhIjoiY2o5cm50cmw3MDE5cjJ4cGM2aWpud2lkMCJ9.dG-9XfpHOuE6FzQdRfa5Og';
 	}
 
-  ionViewDidLoad() {
-	this.map = this.initMap();
+	ionViewDidLoad() {
+		this.map = this.initMap();
 
-	this.getCurrentLocation().subscribe(location => {
-		 this.centerLocation(location);
-		 this.setDirections(location);
-	});
+		this.getCurrentLocation().subscribe(location => {
+			this.centerLocation(location);
+			this.setDirections(location);
+		});
 		this.setMarkers();
 
 	}
 
-	setMarkers(){
+	setMarkers() {
 		var arr = [];
 		var map = this.map;
 
@@ -50,184 +50,186 @@ export class HomePage {
 		// });	
 
 		this.afdb.list('location').snapshotChanges().subscribe(data => {
-			for (var i = 0 ; i < data.length; i++) {
+			for (var i = 0; i < data.length; i++) {
 				// console.log()
-				// arr.push(data[i]);
+				arr.push(data[i]);
 				//  console.log(data[i]);
-				// console.log(data[i].key);
+				console.log(data[i].payload.val());
 			}
 
-		
-//gumawa ng array ng di
-			const popup = new mapboxgl.Popup();	
+
+			//gumawa ng array ng di
+			const popup = new mapboxgl.Popup();
 
 			for (var i = 0; i < arr.length; i++) {
 				popup.setHTML('<h1>Loakan namba wan!</h1>');
 				var el = document.createElement('div');
 				el.innerHTML = "You are here!";
-				//console.log(arr[i].payload.val());
-				el.id = arr[i].key;
-				console.log(el.id);
-				var coords = new mapboxgl.LngLat(122.009,20.457);
-				 new mapboxgl.Marker(el,{offset:[-25,-25]})
-				.setLngLat(coords)
-				.setPopup(popup)
-				.addTo(map);
-				  	
-			el.addEventListener('click', () => { 
-				let actionSheet = this.actionSheetCtrl.create({
-				title: 'Name of place',
-				buttons: [
 
-					{
-					text: 'Request',
-					role: 'destructive',
-					handler: () => {
-						console.log('Destructive clicked');
-					}
-					},{
-					text: 'More Details',
-					handler: () => {
-						var tmp = data[i].key;
-						this.navCtrl.push(ComoredetailsPage, tmp);
-					}
-					},{
-					text: 'Cancel',
-					role: 'cancel',
-					handler: () => {
-						console.log('Cancel clicked');
-						popup.remove();
-					}
-					}
-				]
-				});
-				actionSheet.present();
+				el.id = 'marker' +i;
+				console.log(el.id);
+				var coords = new mapboxgl.LngLat(data[i].payload.val().lng, data[i].payload.val().lat);
+				console.log(coords);
+				new mapboxgl.Marker(el, { offset: [-25, -25] })
+					.setLngLat(coords)
+					.setPopup(popup)
+					.addTo(map);
+
+				el.addEventListener('click', (e) => {
+					console.log(e);
+					let actionSheet = this.actionSheetCtrl.create({
+						title: 'Name of place',
+						buttons: [
+
+							{
+								text: 'Request',
+								role: 'destructive',
+								handler: () => {
+									console.log('Destructive clicked');
+								}
+							}, {
+								text: 'More Details',
+								handler: () => {
+									// var tmp = data[i].key;
+									this.navCtrl.push(ComoredetailsPage);
+								}
+							}, {
+								text: 'Cancel',
+								role: 'cancel',
+								handler: () => {
+									console.log('Cancel clicked');
+									popup.remove();
+								}
+							}
+						]
+					});
+					actionSheet.present();
 				});
 			}
-			});
-			
+		});
+
 	}
 
-  	setDirections(location){
-	const directions = new MapboxDirections({
-		accessToken: mapboxgl.accessToken,
-		interactive:false
-	});
+	setDirections(location) {
+		const directions = new MapboxDirections({
+			accessToken: mapboxgl.accessToken,
+			interactive: false
+		});
 
-	this.map.addControl(directions, 'top-left');
+		this.map.addControl(directions, 'top-left');
 
-	directions.setOrigin(location.lng + ','+location.lat);
+		directions.setOrigin(location.lng + ',' + location.lat);
 
-  }
+	}
 
-  initMap(location = new mapboxgl.LngLat(120.5960,16.4023)){
+	initMap(location = new mapboxgl.LngLat(120.5960, 16.4023)) {
 
-	let map = new mapboxgl.Map({
-	container: 'map',
-	style: 'mapbox://styles/mapbox/streets-v10',
-	center: location,
-	zoom: 5,
-  	attributionControl: false,
-	});
+		let map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/mapbox/streets-v10',
+			center: location,
+			zoom: 5,
+			attributionControl: false,
+		});
 
-	return map;
-  }
+		return map;
+	}
 
-  //returns latlng coordinates
-  getCurrentLocation(){
+	//returns latlng coordinates
+	getCurrentLocation() {
 
-  		let loading = this.loadingCtrl.create({
-  			content:'Locating...'
-  		});
+		let loading = this.loadingCtrl.create({
+			content: 'Locating...'
+		});
 
-  		loading.present(loading);
+		loading.present(loading);
 
-  		let options = {
-  			timeout: 10000, 
-  			enableHighAccuracy: true
-  		};
+		let options = {
+			timeout: 10000,
+			enableHighAccuracy: true
+		};
 
-  		let locationsObs = Observable.create(observable =>{
-	  		this.geolocation.getCurrentPosition(options)
-	  		.then(resp => {
-	  			let lat = resp.coords.latitude;
-	  			let lng = resp.coords.longitude;
+		let locationsObs = Observable.create(observable => {
+			this.geolocation.getCurrentPosition(options)
+				.then(resp => {
+					let lat = resp.coords.latitude;
+					let lng = resp.coords.longitude;
 
 
-	  			let location = new mapboxgl.LngLat(lng,lat);
+					let location = new mapboxgl.LngLat(lng, lat);
 
-	  			observable.next(location);
-	  			loading.dismiss();
+					observable.next(location);
+					loading.dismiss();
 
-	  		}).catch(error => {
-	  			console.log('Error getting location', error);
-	  			loading.dismiss();
-	  		});
-  		});
+				}).catch(error => {
+					console.log('Error getting location', error);
+					loading.dismiss();
+				});
+		});
 
-  		return locationsObs;
-  }
+		return locationsObs;
+	}
 
-  centerLocation(location){
-  	if (location){
-  		this.map.panTo(location);
-  		this.addMarker(location);
-  	}else{
-  		this.getCurrentLocation().subscribe(currentLocation => {
-			this.map.panTo(currentLocation);
-			this.marker.remove();
-			this.addMarker(currentLocation);
-  		});
-  	}
-  }
+	centerLocation(location) {
+		if (location) {
+			this.map.panTo(location);
+			this.addMarker(location);
+		} else {
+			this.getCurrentLocation().subscribe(currentLocation => {
+				this.map.panTo(currentLocation);
+				this.marker.remove();
+				this.addMarker(currentLocation);
+			});
+		}
+	}
 
-  addMarker(location){
-  	if (location) {
-		//1 this.marker = new mapboxgl.Marker(el, {offset:[-25, -25]})
-		this.marker = new mapboxgl.Marker()
-		.setLngLat(location)
-		//2 .setPopup(popup)
-		.addTo(this.map);
-  	}else{
-  		console.log('No coordinates!');
-  	}
+	addMarker(location) {
+		if (location) {
+			//1 this.marker = new mapboxgl.Marker(el, {offset:[-25, -25]})
+			this.marker = new mapboxgl.Marker()
+				.setLngLat(location)
+				//2 .setPopup(popup)
+				.addTo(this.map);
+		} else {
+			console.log('No coordinates!');
+		}
 
-  	//3MARKER POPUP / MORE DETAILS
-	// const popup = new mapboxgl.Popup()
- //    .setHTML('<h1>Loakan namba wan!</h1>');
-	
-	// var el = document.createElement('div');
-	// el.innerHTML = "You are here!";
-	// el.id = 'marker';
+		//3MARKER POPUP / MORE DETAILS
+		// const popup = new mapboxgl.Popup()
+		//    .setHTML('<h1>Loakan namba wan!</h1>');
 
- //  	el.addEventListener('click', () => { 
+		// var el = document.createElement('div');
+		// el.innerHTML = "You are here!";
+		// el.id = 'marker';
 
-	//     let actionSheet = this.actionSheetCtrl.create({
-	//       title: 'Name of place',
-	//       buttons: [
-	//         {
-	//           text: 'Request',
-	//           role: 'destructive',
-	//           handler: () => {
-	//             console.log('Destructive clicked');
-	//           }
-	//         },{
-	//           text: 'More Details',
-	//           handler: () => {
-	//             this.navCtrl.push(ComoredetailsPage);
-	//           }
-	//         },{
-	//           text: 'Cancel',
-	//           role: 'cancel',
-	//           handler: () => {
-	//             console.log('Cancel clicked');
-	//             popup.remove();
-	//           }
-	//         }
-	//       ]
-	//     });
-	//     actionSheet.present();
+		//  	el.addEventListener('click', () => { 
 
- //  	}); 
-  }
+		//     let actionSheet = this.actionSheetCtrl.create({
+		//       title: 'Name of place',
+		//       buttons: [
+		//         {
+		//           text: 'Request',
+		//           role: 'destructive',
+		//           handler: () => {
+		//             console.log('Destructive clicked');
+		//           }
+		//         },{
+		//           text: 'More Details',
+		//           handler: () => {
+		//             this.navCtrl.push(ComoredetailsPage);
+		//           }
+		//         },{
+		//           text: 'Cancel',
+		//           role: 'cancel',
+		//           handler: () => {
+		//             console.log('Cancel clicked');
+		//             popup.remove();
+		//           }
+		//         }
+		//       ]
+		//     });
+		//     actionSheet.present();
+
+		//  	}); 
+	}
 }
