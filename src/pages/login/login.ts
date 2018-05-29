edbc3ab2d7e73a1ae1be288895f7737ee7d4c443
aldrienne <aldriennej@gmail.com>
2018-05-29 18:34:20 +0800

    Routing from login to home pages if user exists

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -26,9 +26,24 @@ export class LoginPage {
     private toastCtrl:ToastController,
     private loadingCtrl:LoadingController) {
 
-      if(this.authProvider.setID){
-        console.log('there id exists');
+      try{
+        if(this.afs.auth.currentUser.uid){
+          this.afdb.object<any>('profile/' + this.afs.auth.currentUser.uid).valueChanges().take(1).subscribe(data=>{
+            if(data.role == 3) {
+              this.navCtrl.setRoot("CoHomePage");
+            } else if (data.carowner && (data.role == 1 || data.role == undefined)) {
+              this.navCtrl.setRoot("CoHomePage");
+            } else if (data.homeowner && (data.role == 2 || data.role == undefined)) {
+              this.navCtrl.setRoot("HoHomePage");
+            }else if(data.establishment && (data.role == 4 || data.role == undefined)){
+              this.navCtrl.setRoot("EstHomePage");
+            } 
+          });
+        }
+      }catch(e){
+        console.log(e);
       }
+      
   }
 
   ionViewDidLoad() {
@@ -45,16 +60,6 @@ export class LoginPage {
 
     });
   }
-
-  removeDuplicates(arr){
-    let unique_array = []
-    for(let i = 0;i < arr.length; i++){
-        if(unique_array.indexOf(arr[i]) == -1){
-            unique_array.push(arr[i])
-        }
-    }
-    return unique_array
-}
   
   showToast(message) {
     let toast = this.toastCtrl.create({
